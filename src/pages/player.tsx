7779a18736977e6fd3392139ea8a42080229fb34
andersonkaiti <anderkaiti@gmail.com>
2025-10-19 00:29:14 -0300

    :lipstick: styles(player): enhance player layout with a new sidebar for course modules and improved styling

--- src/pages/player.tsx
+++ src/pages/player.tsx
@@ -23,7 +23,7 @@ export function Player() {
           </button>
         </div>
 
-        <main className="relative flex overflow-hidden rounded-lg border border-zinc-800 bg-zinc-900 shadow">
+        <main className="relative flex overflow-hidden rounded-lg border border-zinc-800 bg-zinc-900 pr-80 shadow">
           <div className="flex-1">
             <div className="aspect-video w-full bg-zinc-950">
               <ReactPlayer
@@ -35,7 +35,87 @@ export function Player() {
             </div>
           </div>
 
-          <aside className="w-80 border-zinc-800 border-l bg-zinc-900">
+          <aside className="scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-800 absolute top-0 right-0 bottom-0 w-80 divide-y-2 divide-zinc-900 overflow-y-scroll border-zinc-800 border-l bg-zinc-900">
+            <div>
+              <button
+                className="flex w-full items-center gap-3 bg-zinc-800 p-4"
+                type="button"
+              >
+                <div className="flex size-10 items-center justify-center rounded-full bg-zinc-950 text-xs">
+                  1
+                </div>
+
+                <div className="flex flex-col gap-1 text-left">
+                  <strong className="text-sm">Desvendando o Redux</strong>
+
+                  <span className="text-xs text-zinc-400">12 aulas</span>
+                </div>
+
+                <ChevronDown className="ml-auto size-4" />
+              </button>
+            </div>
+            <div>
+              <button
+                className="flex w-full items-center gap-3 bg-zinc-800 p-4"
+                type="button"
+              >
+                <div className="flex size-10 items-center justify-center rounded-full bg-zinc-950 text-xs">
+                  1
+                </div>
+
+                <div className="flex flex-col gap-1 text-left">
+                  <strong className="text-sm">Desvendando o Redux</strong>
+
+                  <span className="text-xs text-zinc-400">12 aulas</span>
+                </div>
+
+                <ChevronDown className="ml-auto size-4" />
+              </button>
+
+              <nav className="relative flex flex-col gap-4 p-6">
+                <button
+                  className="flex items-center gap-3 text-sm text-zinc-400"
+                  type="button"
+                >
+                  <VideoIcon className="size-4 text-zinc-500" />
+                  <span>Fundamentos do Redux</span>
+                  <span className="ml-auto font-mono text-xs text-zinc-500">
+                    09:13
+                  </span>
+                </button>
+              </nav>
+            </div>
+            <div>
+              <button
+                className="flex w-full items-center gap-3 bg-zinc-800 p-4"
+                type="button"
+              >
+                <div className="flex size-10 items-center justify-center rounded-full bg-zinc-950 text-xs">
+                  1
+                </div>
+
+                <div className="flex flex-col gap-1 text-left">
+                  <strong className="text-sm">Desvendando o Redux</strong>
+
+                  <span className="text-xs text-zinc-400">12 aulas</span>
+                </div>
+
+                <ChevronDown className="ml-auto size-4" />
+              </button>
+
+              <nav className="relative flex flex-col gap-4 p-6">
+                <button
+                  className="flex items-center gap-3 text-sm text-zinc-400"
+                  type="button"
+                >
+                  <VideoIcon className="size-4 text-zinc-500" />
+                  <span>Fundamentos do Redux</span>
+                  <span className="ml-auto font-mono text-xs text-zinc-500">
+                    09:13
+                  </span>
+                </button>
+              </nav>
+            </div>
             <div>
               <button
                 className="flex w-full items-center gap-3 bg-zinc-800 p-4"
